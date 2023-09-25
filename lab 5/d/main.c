4a8cb03b4d85e278dcc25032bc5181783eb67214
Vaishnavi Upadhya H <73239633+vaishnavi-upadhya@users.noreply.github.com>
2023-09-25 15:55:37 +0530

    Add files via upload

--- lab 5/d/main.c
+++ lab 5/d/main.c
@@ -1,32 +1,65 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdint.h>
 
-typedef struct {
-    int x;
-    int y;
-} Point;
 
-int a, b, p; // Curve parameters
 
-// Function to check if a point lies on the curve
-int isPointOnCurve(Point point) {
-    return (int)(pow(point.y, 2) - (pow(point.x, 3) + a * point.x + b)) % p == 0;
+// Function to calculate the modular inverse
+int modInverse(int a, int m) {
+    int m0 = m, t, q;
+    int x0 = 0, x1 = 1;
+
+    if (m == 1)
+        return 0;
+
+    while (a > 1) {
+        q = a / m;
+        t = m;
+        m = a % m, a = t;
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+
+    if (x1 < 0)
+        x1 += m0;
+
+    return x1;
+}
+
+// Function to check if a number is a quadratic residue modulo p
+int isQuadraticResidue(int x,int p) {
+    for (int i = 0; i < p; i++) {
+        if ((i * i) % p == x)
+            return 1;
+    }
+    return 0;
+}
+
+// Function to find all points on the elliptic curve
+void findPointsOnCurve(int p, int a,int b) {
+   for (int x = 0; x < p; x++) {
+        int y_squared = (x * x * x + a * x + b) % p;
+        if (isQuadraticResidue(y_squared,p)) {
+            int y = 0;
+            while ((y * y) % p != y_squared) {
+                y++;
+            }
+             printf("(%d, %d)\n", x, y);
+             printf("(%d, %d)\n",x , p-y);
+        }
+    }
 }
 
 int main() {
-    printf("Enter the parameters for the elliptic curve (a, b, p): ");
-    scanf("%d %d %d", &a, &b, &p);
-
-    printf("Points on the elliptic curve:\n");
-
-    for (int x = 0; x < p; x++) {
-        int y_squared = (int)(pow(x, 3) + a * x + b);
-        int y = (int)(sqrt(abs(y_squared))) % p;
-
-        if (isPointOnCurve((Point){x, y}) || isPointOnCurve((Point){x, -y})) {
-            printf("(%d, %d)\n", x, y);
-        }
-    }
-
+    int a,b,p;
+    printf("Enter the value of p:" );
+    scanf("%d", &p);
+    printf("Enter the value of a:" );
+     scanf("%d", &a);
+    printf("Enter the value of b:" );
+     scanf("%d", &b);
+    printf("Points on the elliptic curve y^2 = x^3 + %dx + %d (mod %d):\n", a, b, p);
+    findPointsOnCurve(p,a,b);
     return 0;
 }
+
